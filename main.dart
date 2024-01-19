e19465327ad13ce75279b11eded1fc9c11067c6f
aleena02122002 <khananna919@gmail.com>
2024-01-19 23:30:57 +0500

    Console based application

--- main.dart
+++ main.dart
@@ -24,7 +24,7 @@ void main() {
     if (choice?.toLowerCase() == "home") {
       homePage();
     } else if (choice?.toLowerCase() == "setting") {
-      signOut();
+      setting();
     } else {
       print("error");
     }
--- setting.dart
+++ setting.dart
@@ -1,6 +1,15 @@
 import 'login.dart';
+import 'userAccount.dart';
 
+String? answers;
 bool setting() {
+  print("");
+  print("Account Details     ||     Sign Out     ");
+  print("");
+
+  if (answers?.toLowerCase() == "account") {
+    userAccount();
+  }
   return true;
 }
 
